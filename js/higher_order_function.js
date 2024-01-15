234198bd8eb5d050fca63b45de19ba6218852706
Saifur Rahman <sr018695@gmail.com>
2024-01-15 07:03:40 +0600

    Higher order implemented which make the code just 5 lines of a 20 lines of code

--- js/higher_order_function.js
+++ js/higher_order_function.js
@@ -11,46 +11,48 @@
 * - Hoisting 
 */
 
-function generateTwoRandomNum(max){
+function generateTwoRandomNum(max, WhatDoYouWant){
   const random1 = Math.floor(Math.random()*max);
   const random2 = Math.floor(Math.random()*max);
-
-  return {random1,random2};
+  const result = WhatDoYouWant(random1, random2);
+  return result;
 }
+generateTwoRandomNum(100, (rand1,rand2)=>console.log(rand1,rand2));
+generateTwoRandomNum(100, (r,b)=>console.log(r+b));
 
-function randomSum(max){
-  // const random1 = Math.floor(Math.random()*max);
-  // const random2 = Math.floor(Math.random()*max);
-  const {random1,random2} = generateTwoRandomNum(max);
+// function randomSum(max){
+//   // const random1 = Math.floor(Math.random()*max);
+//   // const random2 = Math.floor(Math.random()*max);
+//   const {random1,random2} = generateTwoRandomNum(max);
 
-  return random1 + random2;
-}
+//   return random1 + random2;
+// }
 
-function randomSub(max){
-  // const random1 = Math.floor(Math.random()*max);
-  // const random2 = Math.floor(Math.random()*max);
-  const {random1,random2} = generateTwoRandomNum(max);
+// function randomSub(max){
+//   // const random1 = Math.floor(Math.random()*max);
+//   // const random2 = Math.floor(Math.random()*max);
+//   const {random1,random2} = generateTwoRandomNum(max);
   
-  return random2-random1;
-}
+//   return random2-random1;
+// }
 
-function randomMultiple(max){
-  // const random1 = Math.floor(Math.random()*max);
-  // const random2 = Math.floor(Math.random()*max);
-  const {random1,random2} = generateTwoRandomNum(max);
+// function randomMultiple(max){
+//   // const random1 = Math.floor(Math.random()*max);
+//   // const random2 = Math.floor(Math.random()*max);
+//   const {random1,random2} = generateTwoRandomNum(max);
 
-  return random1*random2;
-}
+//   return random1*random2;
+// }
 
-function randomSquare(max){
-  // const random1 = Math.floor(Math.random()*max);
-  // const random2 = Math.floor(Math.random()*max);
-  const {random1,random2} = generateTwoRandomNum(max);
+// function randomSquare(max){
+//   // const random1 = Math.floor(Math.random()*max);
+//   // const random2 = Math.floor(Math.random()*max);
+//   const {random1,random2} = generateTwoRandomNum(max);
 
-  return random1*random1+random2*random2;
-}
+//   return random1*random1+random2*random2;
+// }
 
-console.log(randomSum(100));
-console.log(randomSub(100));
-console.log(randomMultiple(100));
-console.log(randomSquare(100));
+// console.log(randomSum(100));
+// console.log(randomSub(100));
+// console.log(randomMultiple(100));
+// console.log(randomSquare(100));
