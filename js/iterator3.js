fe51e70e73f35f1be7c3bf120528f339e0b3135d
Saifur Rahman <sr018695@gmail.com>
2024-01-16 17:23:35 +0600

    making an object iterable using closure and Symbol.iterator()

---
+++ js/iterator3.js
@@ -0,0 +1,34 @@
+// We want to loop throw an object but normal object don't allow to iterate, we must make it iteratable if we want make iteratable
+
+const range = {
+  start: 0,
+  stop: 100,
+  step: 5
+};
+
+range[Symbol.iterator] = function () {
+  let current = this.start;
+  const stop = this.stop;
+  const step = this.step;
+  return{
+    next(){
+      const o = {
+        value: current,
+        done: current>stop
+      };
+      current+=step;
+      return o;
+    }
+  }
+}
+for(let v of range){
+  console.log(v);
+}
+const rangeIterator = range[Symbol.iterator]();
+console.log(rangeIterator.next());
+console.log(rangeIterator.next());
+console.log(rangeIterator.next());
+console.log(rangeIterator.next());
+console.log(rangeIterator.next());
+console.log(rangeIterator.next());
+console.log(rangeIterator.next());
