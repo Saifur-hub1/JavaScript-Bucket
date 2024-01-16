762c79c41fc677bb37357bac96247f86de733f37
Saifur Rahman <sr018695@gmail.com>
2024-01-16 16:54:01 +0600

    Iterating to the string with using while loop by using (Symbol.iterator).next()

--- js/iterator1.js
+++ js/iterator1.js
@@ -10,3 +10,26 @@ console.log(iter());
 console.log(iter());
 console.log(iter());
 console.log(iter());
+
+let NAME = 'Saifur Rahman';
+index=0;
+
+function nex(){
+  return (NAME[index++]);
+}
+console.log(nex());
+console.log(nex());
+console.log(nex());
+console.log(nex());
+console.log(nex());
+console.log(nex());
+console.log(nex());
+console.log(nex());
+console.log(nex());
+console.log(nex());
+console.log(nex());
+console.log(nex());
+console.log(nex());
+console.log(nex());
+console.log(nex());
+
