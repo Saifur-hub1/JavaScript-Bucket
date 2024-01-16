c4fad393fbd006f518113cf8d019762cee068862
Saifur Rahman <sr018695@gmail.com>
2024-01-16 16:40:41 +0600

    basic iterator created done

---
+++ js/iterator1.js
@@ -0,0 +1,12 @@
+let arr = [1,2,3,4];
+let index=0;
+function iter(){
+  return arr[index++];
+}
+
+console.log(iter());
+console.log(iter());
+console.log(iter());
+console.log(iter());
+console.log(iter());
+console.log(iter());
